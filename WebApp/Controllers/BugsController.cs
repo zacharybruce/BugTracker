f0c4cea9108ba0b0769923624a4c78e2b1b7e275
Zachary Bruce <zbruce01@gmail.com>
2020-05-28 10:10:52 -0400

    Add Bug controller and views

---
+++ WebApp/Controllers/BugsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.DataAccess;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Authorize]
+    public class BugsController : Controller
+    {
+        private BugTrackerContext db = new BugTrackerContext();
+
+        // GET: Bugs
+        public ActionResult Index()
+        {
+            var bugs = db.Bugs.Include(b => b.Profile);
+            return View(bugs.ToList());
+        }
+
+        // GET: Bugs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bug bug = db.Bugs.Find(id);
+            if (bug == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bug);
+        }
+
+        // GET: Bugs/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProfileID = new SelectList(db.Profiles, "ID", "Email");
+            return View();
+        }
+
+        // POST: Bugs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,BugName,Priority,BugDescription,ProfileID")] Bug bug)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bugs.Add(bug);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProfileID = new SelectList(db.Profiles, "ID", "Email", bug.ProfileID);
+            return View(bug);
+        }
+
+        // GET: Bugs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bug bug = db.Bugs.Find(id);
+            if (bug == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProfileID = new SelectList(db.Profiles, "ID", "Email", bug.ProfileID);
+            return View(bug);
+        }
+
+        // POST: Bugs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,BugName,Priority,BugDescription,ProfileID")] Bug bug)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bug).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProfileID = new SelectList(db.Profiles, "ID", "Email", bug.ProfileID);
+            return View(bug);
+        }
+
+        // GET: Bugs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bug bug = db.Bugs.Find(id);
+            if (bug == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bug);
+        }
+
+        // POST: Bugs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bug bug = db.Bugs.Find(id);
+            db.Bugs.Remove(bug);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
